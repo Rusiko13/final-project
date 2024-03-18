81257d6abc592171e4ca37ea0c06fb8be4fc620f
Rusiko13 <rusochlaidze@gmail.com>
2024-03-18 20:47:33 +0400

    almost ready need some js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ document
     console.log("password" + password);
     document.getElementById("email").value = "";
     document.getElementById("password").value = "";
-    alert`you signed up successfully, Thank you! 
+    alert`you signed up successfully, Thank you!
     Now try to log in `;
     signupFormContainer.style.display = "none";
     loginFormContainer.style.display = "block";
@@ -30,6 +30,43 @@ signupButton.addEventListener("click", function () {
     pageBackground.classList.remove("background");
   }
 });
+
+//password icon signup
+
+let passwInput = document.getElementById("password1");
+let passwIcon = document.getElementById("passw-eye");
+passwIcon.addEventListener("click", function () {
+  if (passwInput.type == "password") {
+    passwInput.setAttribute("type", "text");
+    passwIcon.classList.add("fa-eye-slash");
+    passwIcon.classList.remove("fa-eye");
+  } else {
+    passwInput.setAttribute("type", "password");
+    passwIcon.classList.add("fa-eye");
+    passwIcon.classList.remove("fa-eye-slash");
+  }
+});
+
+//email validation signup
+let emailSign = document.getElementById("emailS");
+function validationSign() {
+  let emailVal = document.getElementById("emailS").value;
+  let errorPar = document.getElementById("textError");
+  let emailPattern1 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailVal.match(emailPattern1)) {
+    errorPar.textContent = "your email is valid";
+    errorPar.style.color = "green";
+  } else {
+    errorPar.textContent = "your email is invalid";
+    errorPar.style.color = "red";
+  }
+  if (emailVal == "") {
+    textError.innerHTML = " ";
+  }
+}
+emailSign.addEventListener("keyup", validationSign);
+
+//loginform
 
 document
   .getElementById("loginForm")
@@ -66,49 +103,91 @@ loginButton.addEventListener("click", function () {
   }
 });
 
-// let btn = document.getElementById("groom-btn1");
-// btn.addEventListener("click", function () {
-//   document.body.classList.toggle("body-class");
-// });
+//password icon login
 
-// let input = document.getElementById("email");
-// input.addEventListener("keyup", function () {
-//   input.style.border = "2px solid green";
-// });
-
-// let divIcon = document.querySelector(".icon-div");
-// divIcon.addEventListener("mouseover", function (e) {
-//   e.divIcon.style.backgroundColor = "red";
-// });
-function bookAppointment() {
-  const date = document.getElementById("date").value;
-  const time = document.getElementById("time").value;
-  alert("Appointment booked for " + date + " at " + time);
-}
-
-// let iconDiv = document.querySelector(".icon-div");
-// iconDiv.addEventListener("click", function () {
-//    this.classList.toggle("active-color");
-//   });
-
-let subscribeForm = document.getElementById("subscribeForm");
-
-subscribeForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let input = document.getElementById("email");
-  let inputValue = input.value;
-  alert`you subscribed successfully, Thank you!`;
-  inputValue.value = " ";
-  //ინფუთს ვერ ვასუფთავებ
+let passwordInput = document.getElementById("password");
+let passwordEye = document.getElementById("password-eye");
+passwordEye.addEventListener("click", function () {
+  if (passwordInput.type == "password") {
+    passwordInput.setAttribute("type", "text");
+    passwordEye.classList.remove("fa-eye");
+    passwordEye.classList.add("fa-eye-slash");
+  } else {
+    passwordInput.setAttribute("type", "password");
+    passwordEye.classList.add("fa-eye");
+    passwordEye.classList.remove("fa-eye-slash");
+  }
 });
+
+//email validation login
+
+let emailLogin = document.getElementById("email");
+function validation() {
+  let emailValue = document.getElementById("email").value;
+  let errorP = document.getElementById("error-email");
+  let emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailValue.match(emailPattern)) {
+    errorP.textContent = "your email is valid";
+    errorP.style.color = "green";
+  } else {
+    errorP.textContent = "your email is invalid";
+    errorP.style.color = "red";
+  }
+  if (emailValue == "") {
+    textError.innerHTML = " ";
+  }
+}
+emailLogin.addEventListener("keyup", validation);
+//user icon home page, inside elements
 
 let userIcon = document.querySelector(".user-div");
-let userUl =  document.querySelector(".user-ul");
+let userUl = document.querySelector(".user-ul");
 userIcon.appendChild(userUl);
-userIcon.addEventListener("click", function(){
-userUl.classList.toggle("user-ul-active");
+userIcon.addEventListener("click", function () {
+  userUl.classList.toggle("user-ul-active");
 });
 
-    // Proceed with form submission if all checks pass
-    return true;
-}
+//adopt page loved icons paw in a heart
+
+let iconsActive = document.querySelectorAll(".icons-active");
+iconsActive.forEach(function (element) {
+  element.addEventListener("click", function () {
+    element.classList.toggle("active-red");
+  });
+});
+
+// // let btn = document.getElementById("groom-btn1");
+// // btn.addEventListener("click", function () {
+// //   document.body.classList.toggle("body-class");
+// // });
+
+// // let input = document.getElementById("email");
+// // input.addEventListener("keyup", function () {
+// //   input.style.border = "2px solid green";
+// // });
+
+// // let divIcon = document.querySelector(".icon-div");
+// // divIcon.addEventListener("mouseover", function (e) {
+// //   e.divIcon.style.backgroundColor = "red";
+// // });
+// function bookAppointment() {
+//   const date = document.getElementById("date").value;
+//   const time = document.getElementById("time").value;
+//   alert("Appointment booked for " + date + " at " + time);
+// }
+
+// // let iconDiv = document.querySelector(".icon-div");
+// // iconDiv.addEventListener("click", function () {
+// //    this.classList.toggle("active-color");
+// //   });
+
+// let subscribeForm = document.getElementById("subscribeForm");
+
+// subscribeForm.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   let input = document.getElementById("email");
+//   let inputValue = input.value;
+//   alert`you subscribed successfully, Thank you!`;
+//   inputValue.value = " ";
+//   //ინფუთს ვერ ვასუფთავებ
+// });
